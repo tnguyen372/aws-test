06e999b3024b0e4c2b0888724fe024d30baec3b4
tnguyen372 <tnguyen135@mail.sfsu.edu>
2022-02-16 14:14:07 -0800

    serving html page

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.get(express.static('public'));
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
