e91d8734c56004f7ee9e6d64337e6925af63916a
tnguyen372 <tnguyen135@mail.sfsu.edu>
2022-02-16 14:16:59 -0800

    forgot to change keyword to use for express app

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get(express.static('public'));
+app.use(express.static('public'));
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
